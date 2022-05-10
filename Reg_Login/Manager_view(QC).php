c15cba363914d43e388cb20e9ffe9af6931d0c29
tina1406 <97176209+tina1406@users.noreply.github.com>
2022-05-10 18:15:00 +0200

    Manager View (still editing)

---
+++ Reg_Login/Manager_view(QC).php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Manager View</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Google font -->
+         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+          <!-- Bootstrap -->
+        <link type="text/css" rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+        <!--top nav style-->
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="../assets/css/MainStyles.css">
+
+    </head>
+    
+   <!-- <body style="background-image:url('../assets/img/registerBg.jpg')">-->
+   <body style="background-image:url('../assets/img/registerBg.jpg')">
+
+   <div id="booking" class="section">
+        <div class="section-center">
+            <div class="container">
+                <div class="row">
+                </div>
+                
+    <div class="topnav" id="mtTopnav">
+            <a href="Manager_view(QC).php">Home</a>   
+        </div>
+    </body>
+</html>
