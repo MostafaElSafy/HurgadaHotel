7063aaf1cebdbf3bf78e65152169acff0294e29e
MartinaMilad20 <git>
2022-05-12 01:17:46 +0200

    Show/hide table buttons(Incomplete)

--- Manager_view/Manage_Rooms.php
+++ Manager_view/Manage_Rooms.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Manager View</title>
+    <title>Manage Rooms</title>
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
     <!-- Bootstrap -->
@@ -49,9 +49,156 @@
             document.body.style.backgroundColor = "white";
         }
     </script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
+
+<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+
+<script>
+var xValues = ["Sea View ", "Garden View", "Others"];
+var yValues = [70, 49, 44];
+var barColors = [
+  "#87CEEB",
+  "#355E3B",
+  "	#DEB887"
+];
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Most common Booked Room Views Chart"
+    }
+  }
+});
+
+</script>
+
+ <!-- Buttons for tables -->
+ <h3> Ratings and Reviews </h3>
+
+ <script>
+  function showTable()
+  {
+    document.getElementsByClassName('table table-bordered table-striped table-hover')[0].style.visibility = "visible";
+  }
+  function hideTable()
+  {
+   document.getElementsByClassName('table table-bordered table-striped table-hover')[0].style.visibility = "hidden";
+  }
+</script>
+<body onload="javascript:hideTable()">
+<input type='button' onClick='javascript:showTable();' value='show'>
+<input type='button' onClick='javascript:hideTable();' value='hide'>
+<table class='table table-bordered table-striped table-hover '>
+
+
+<style>
+.head{
+                background-color:lightskyblue;
+                color:white;
+     }
+</style>
+        <tr class="head">
+            <th>Guest_ID</th>
+            <th>Room_ID</th>
+            <th>Rating_ID</th>
+            <th>Rating_Room</th>
+            <th>Reviews</th>
+        </tr>
+        <tbody>
+        <tr>
+            <td>00152</td>
+            <td>20</td>
+            <td>5-123</td>
+            <td>5</td>
+            <td>This Hotel is very unique </td>
+
+
+        </tr>
+        <tr>
+            <td>00153</td>
+            <td>25</td>
+            <td>4-123</td>
+            <td>4</td>
+            <td>This Hotel is awsome </td>
+        </tr>
+
+        <tr>
+        <td>00154</td>
+            <td>28</td>
+            <td>2-123</td>
+            <td>2</td>
+            <td>Bad experiance  </td>
+        </tr>
+        </tbody>
+      </table>
+
+    
+
+
+
+      <h3>  Edits and Cancellation </h3>
+      <script>
+  function showTable()
+  {
+    document.getElementsByClassName('table table-bordered table-striped table-hover')[0].style.visibility = "visible";
+  }
+  function hideTable()
+  {
+   document.getElementsByClassName('table table-bordered table-striped table-hover')[0].style.visibility = "hidden";
+  }
+</script>
+<body onload="javascript:hideTable()">
+<input type='button' onClick='javascript:showTable();' value='show'>
+<input type='button' onClick='javascript:hideTable();' value='hide'>
+      <table class="table table-bordered table-striped table-hover ">
+<style>
+.head{
+                background-color:lightskyblue;
+                color:white;
+     }
+</style>
+        <tr class="head">
+            <th>Guest_ID</th>
+            <th>Edits</th>
+            <th>Cancellation</th>
+        </tr>
+        <tbody>
+        <tr>
+            <td>00152</td>
+            <td>need lamp </td>
+            <td>no</td>
+          
+
+
+        </tr>
+        <tr>
+            <td>00153</td>
+            <td>nothing</td>
+            <td>x</td>
+          
+        </tr>
+
+        <tr>
+        <td>00154</td>
+            <td>need sofa</td>
+            <td>no</td>
+      
+        </tr>
+        </tbody>
+
+      </table>
 </body>
 
 </html>
-</body>
 
-</html>
