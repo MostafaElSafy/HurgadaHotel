03e5cdbb5d2005301e8f95ec16b56f9ce219d0f1
MartinaMilad20 <git>
2022-05-25 17:40:33 +0200

    Manage Receptionist(2) to be continued

--- Manager_view/Manage_Receptionist.php
+++ Manager_view/Manage_Receptionist.php
@@ -145,7 +145,7 @@
 
 <div class="modal-footer">
     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-    <button type="button" class="btn btn-primary">Save </button>
+    <button type="button" class="btn btn-primary"> Save </button>
 </div>
 
 
