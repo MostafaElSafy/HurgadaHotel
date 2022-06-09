3c66e33f3269283cf99453d91f022f276c816569
AE307 <a_elsayed_f@icloud.com>
2022-06-09 22:58:32 +0200

    mange request

--- Manager_view/Manage_Request.php
+++ Manager_view/Manage_Request.php
@@ -37,8 +37,76 @@ include("../functions.php");
   <h4>Guests Requests</h4>
 
   <hr>
+  <style>
+        .table td {
+            text-align: center;
+        }
+    </style>
+    <style>
+        .table th {
+            text-align: center;
+        }
+    </style>
 
-  <div class="tab-pane fade" id="nav-Request" role="tabpanel" aria-labelledby="nav-Request-tab"></div>
+    <div class="tab-pane fade" id="nav-Request" role="tabpanel" aria-labelledby="nav-Request-tab"></div>
+    <table class="table table-striped table-hover">
+        <thead style="background-color: #0096c7 ; color:  rgba(242, 242, 242, 0.982);; font-weight:500;">
+            <tr>
+
+            <th scope="col"> First Name</th>
+            <th scope="col"> Last Name</th>
+            <th scope="col">E mail</th>
+            <th scope="col"> Password</th>
+            <th scope="col">Date of Birth</th>
+            <th scope="col"> Extra Members</th>
+            <th scope="col">Guest ID </th>
+            <th scope="col"> Phone Number </th>
+            <th scope="col"> Nationality </th>
+            <th scope="col"> state </th>
+            <th scope="col"> Actions </th>
+
+
+            </tr>
+        </thead>
+
+        <tbody id="myTable">
+            <!--Connecting the table that appears to the receptionist with the database-->
+            <?php
+            $conn = new mysqli("localhost", "root", "", "hurghada_db");
+            if (!$conn) {
+                die("Connection Failed: " . mysqli_connect_error());
+            }
+
+            $query = "SELECT * FROM guest ";
+            $result = $conn->query($query);
+            if (!$result) {
+                die("Fatal Query error");
+            }
+
+            $rows = $result->num_rows;
+
+            for ($j = 0; $j < $rows; $j++) {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                $guestid = $row['guest_id'];
+
+                echo '<tr><td> ' . $row['firstname'] . '</td> <td> ' . $row['lastname'] . '</td><td>' . $row['email'] . ' </td> <td> ' . $row['password'] . '</td><td> ' . $row['dateofbirth'] . '</td><td> ' . $row['extramembers'] . '</td><td> ' . $row['guest_id'] . '</td><td> ' . $row['phonenumber'] . '</td><td> ' . $row['nationality'] . '</td><td> ' . $row['state'] . '</td>';
+
+                echo '<td>
+              <div class="row">
+               <div class="col">
+               <div><a href="acceptQC.php?acceptid=' . $guestid . '" class="btn btn-primary btn-sm">Accept</a>
+                    <a href="rejectQC.php?rejectid=' . $guestid . '" class="btn btn-danger btn-sm px-3">Reject</a>
+              </div>
+              </div>
+              </div>
+              </td>';
+                echo "</tr>";
+            }
+            ?>
+
+        </tbody>
+    </table>
+  <!--<div class="tab-pane fade" id="nav-Request" role="tabpanel" aria-labelledby="nav-Request-tab"></div>
   <table class="table table-striped table-hover">
     <thead class="dark1">
       <tr>
@@ -52,27 +120,27 @@ include("../functions.php");
         <th scope="col">Guest ID </th>
         <th scope="col"> Phone Number </th>
         <th scope="col"> Nationality </th>
-        <th scope="col"> Accept </th>
-        <th scope="col"> Reject </th>
+        <th scope="col"> Actions </th>
+        
         <th scope="col"> </th>
       </tr>
     </thead>
 
-    <tbody>
+    <tbody>-->
 
       <!--Connecting the table that appears to the receptionist with the database-->
-      <?php
-      $conn = new mysqli("localhost", "root", "", "hurghada_db");
+      
+     <!-- $conn = new mysqli("localhost", "root", "", "hurghada_db");
       if (!$conn) {
         die("Connection Failed: " . mysqli_connect_error());
       }
 
-      $query = "SELECT firstname, lastname, email, password, datofbirth, extramembers, guest_id, phonenumber, nationality  FROM guest ";
-      $result = $conn->query($query);
+      $query = "SELECT * FROM guest ";
+      $result=mysqli_query($conn,$query);
       if (!$result) {
         die("Fatal Query error");
       }
-
+      
       $rows = $result->num_rows;
 
       for ($j = 0; $j < $rows; $j++) {
@@ -96,7 +164,7 @@ include("../functions.php");
   <div class="modal-footer">
     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
     <button type="button" class="btn btn-primary">Save </button>
-  </div>
+  </div>-->
 
 </body>
 
