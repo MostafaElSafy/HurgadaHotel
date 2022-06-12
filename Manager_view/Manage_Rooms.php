a5f393558b5d4c5fa47d2bda271fa5a47e1e4ee7
Mostafa <MostafaElSafyy@hotmail.com>
2022-06-12 10:10:43 +0200

    react try and manger charts v1

--- Manager_view/Manage_Rooms.php
+++ Manager_view/Manage_Rooms.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,10 +20,10 @@ session_start();
   <!-- SweetAlert2 -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
-
+<link rel="stylesheet" href="dashboard.css.css">
 
   <script src="bower_components/sweetalert2/dist/sweetalert2.min.js"></script>
-<link rel="stylesheet" href="bower_components/sweetalert2/dist/sweetalert2.min.css">
+  <link rel="stylesheet" href="bower_components/sweetalert2/dist/sweetalert2.min.css">
   <!--top nav style-->
   <link rel="stylesheet" type="text/css" href="Manager_styles.css">
   <link rel="stylesheet" type="text/css" href="stars.css">
@@ -34,238 +35,57 @@ session_start();
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.1/Chart.js"></script>
   <?php
   include 'NavBar.html';
- 
+
   ?>
 
 </head>
 
 <body>
   <hr>
-  <div class="container-fluid mt-3">
-
-    <!-- Control the column width, and how they should appear on different devices -->
 
 
-    <div class="row">
 
-      <div class="col-sm-4 ">
-        <h2 class="text-center">CHART</h2>
-        <div class="container">
-          <h6 class="text-center">Number of people rated a specific product 5 stars</h6>
-          <canvas id="barChart" width="250" height="250"></canvas>
-        </div>
-        <script>
-          jQuery(document).ready(function() {
-            var chartDiv = $("#barChart");
-            var myChart = new Chart(chartDiv, {
-              type: 'pie',
-              data: {
-                labels: ["Toiletries", "Personal Care", "Bathrobs & Slippers", "Coffee Kit"],
-                datasets: [{
-                  data: [21, 39, 10, 14],
-                  backgroundColor: [
-                    "#FF6384",
-                    "#4BC0C0",
-                    "#FFCE56",
-                    "#E7E9ED"
-                  ]
-                }]
-              },
-              options: {
-                title: {
-                  display: true,
-                  text: '5 Stars rated Products'
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-              }
-            });
-          });
-        </script>
+  <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
-         <!--BATABASE CONNECTION-->
+  <h2>Section title</h2>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Header</th>
+          <th scope="col">Header</th>
+          <th scope="col">Header</th>
+          <th scope="col">Header</th>
+        </tr>
+      </thead>
+      <tbody>
 
-         <?php
-                     /* $sql = "SELECT product FROM rooms"; //we need to add products in rooms that is rated by the guests
-                    
-                      $result = $conn->query($sql);
-                      while($row = mysqli_fetch_object($result)){ */
-                      ?>
-        <h6>The Product</h6>  <?php //echo $_POST['product'];  ?>
-        
+        <tr>
+          <td>1,010</td>
+          <td>data</td>
+          <td>rich</td>
+          <td>dashboard</td>
+          <td>tabular</td>
+        </tr>
+        <tr>
+          <td>1,011</td>
+          <td>information</td>
+          <td>placeholder</td>
+          <td>illustrative</td>
+          <td>data</td>
+        </tr>
 
-        <p>is the product that got the highest rate by Most of our guests</p>
-      </div>
-      <div class="col-sm-3"></div>
-      <div class="col-sm-4 ">
-        <h2>PRODUCTS RATINGS</h2>
-        <h6>choose rating to view Number of guests and their ratings...</h6>
-
-
-        <div class="rate">
-
-          <input type="radio" id="star5" name="rate" value="5" />
-          <label for="star5" title="text">5 stars</label>
-          <input type="radio" id="star4" name="rate" value="4" />
-          <label for="star4" title="text">4 stars</label>
-          <input type="radio" id="star3" name="rate" value="3" />
-          <label for="star3" title="text">3 stars</label>
-          <input type="radio" id="star2" name="rate" value="2" />
-          <label for="star2" title="text">2 stars</label>
-          <input type="radio" id="star1" name="rate" value="1" />
-          <label for="star1" title="text">1 star</label>
-        </div>
-        <button type="button" class="btn btn-outline-info" onclick="display()">Display Data</button>
-        <div id="result"></div>
-      </div>
-    </div>
-    <script>
-      function display() {
-
-        if (document.getElementById('star5').checked) {
-          document.getElementById("result").innerHTML = document.getElementById("star5").value +
-            " star checked"
-            <?php      ?>
-            ;
-        } else if (document.getElementById('star4').checked) {
-          document.getElementById("result").innerHTML = document.getElementById("star4").value +
-            " stars checked"
-            <?php      ?>
-            ;
-        } else if (document.getElementById('star3').checked) {
-          document.getElementById("result").innerHTML = document.getElementById("star3").value +
-            " stars checked"
-            <?php      ?>
-            ;
-        } else if (document.getElementById('star2').checked) {
-          document.getElementById("result").innerHTML = document.getElementById("star2").value +
-            " stars checked"
-            <?php      ?>
-            ;
-        } else if (document.getElementById('star1').checked) {
-          document.getElementById("result").innerHTML = document.getElementById("star1").value +
-            " stars checked"
-            <?php      ?>
-            ;
-        } else {
-          document.getElementById("disp").innerHTML = "No one selected";
-        }
-      }
-    </script>
-    <br>
-
-    <!-- Or let Bootstrap automatically handle the layout -->
-    <style>
-      .link-muted {
-        color: #aaa;
-      }
-
-      .link-muted:hover {
-        color: #1266f1;
-      }
-    </style>
-    <div class="row">
-      <div class="col-md-8 text-white bg-primary">
-        <div class="container my-1 py-1">
-          <h4 class="mt-5"><img src='../assets/img/chat.png' class='img-responsive iconn3'> Guests Comments </h4>
-          <hr>
-          <div class="row d-flex justify-content-center">
-            <div class="col-md-12 col-lg-10">
-              <div class="card text-dark">
-                <div class="card-body p-4">
-                  <h4 class="mb-0">Recent comments</h4>
-                  <p class="fw-light mb-4 pb-2">Latest Comments section by users</p>
-                  <div class="d-flex flex-start">
-                    <div>
-
-
-                      <!--BATABASE CONNECTION-->
-
-                      <?php
-                     /* $sql = "SELECT guest_id , comments FROM ratingandreviews";
-                    
-                      $result = $conn->query($sql);
-                      while($row = mysqli_fetch_object($result)){ */
-                      ?>
-
-
-                      <h6 class="fw-bold mb-1"><?php // echo $row->guest_id;?></h6>
-                      <div class="d-flex align-items-center mb-3">
-                        <p class="mb-0">
-                          <span class="badge bg-success">submitted</span>
-                        </p>
-
-                      </div>
-                      <p class="mb-0">
-                        <?php //echo $row->comments;?>
-                      </p>
-                      <a href="#!" class="d-flex align-items-center me-3">
-                        <i class="far fa-comment-dots me-2"></i>
-                        <p class="mb-0">Comment</p>
-                      </a>
-                      <?php // } ?>
-                    </div>
-                  </div>
-                </div>
-                
-                <hr class="my-0" />
-              </div>
-            </div>
-          </div>
-         
-          <hr>
-          <label class="form-label" for="addANote" style="color: white;">+ Add a note</label>
-          <input type="text" id="addANote" class="form-control" placeholder="Type comment..." />
-          <div class="d-flex justify-content-between mt-3">
-            <button type="button" class="btn btn-info">
-              Submit Comment
-              <i class="fas fa-long-arrow-alt-right ms-1"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 col-md-4 text-white bg-primary">
-        <h4 class="mt-5"><img src='../assets/img/report.png' class="icon" class='img-responsive iconn3'> General Reports</h4>
-        <hr>
-
-        
-                      <!--BATABASE CONNECTION-->
-
-                      <?php
-                     /* $sql = "SELECT * FROM editsandcancellation";  //han3ml table esmo edits and cancellation a hane7sb count total cancelled times and edits
-                                                                      //most booked room through database 
-                      $result = $conn->query($sql);                     
-                      while($row = mysqli_fetch_object($result)){ */
-                      ?>
-
-        <p>Most booked Room </p>       <?php     ?></i>  
-        <p>Number of Guests cancelled  <?php     ?></p>
-        <p>Number of Guests made edits <?php     ?></p>
-        <hr>
-        <div class="form-outline mb-4">
-          <label class="form-label" for="addANote" style="color: white;">+ Add a note</label>
-          <input type="text" id="addANote" class="form-control" placeholder="Type comment..." />
-        </div>
-
-  
-        <button type="button" id="success" class="btn btn-info">
-    
-          Submit Comment
-        </button>
-        <script>
-       $(document).on('click', '#success', function(e) {
-			swal(
-				'Success',
-				'You clicked the <b style="color:green;">Success</b> button!',
-				'success'
-			)
-		});
-       </script>
-        </div>
-      </div>
-    </div>
+      </tbody>
+    </table>
   </div>
-</div>
+  </main>
+  </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+  <script src="dashboard.js"></script>
 
 </body>
 
