39907ca2972118e3b1f92a28f12d7816c86895ed
Mostafa <MostafaElSafyy@hotmail.com>
2022-06-12 12:54:56 +0200

    logout bug fix

--- Receptionist_page/main_page.php
+++ Receptionist_page/main_page.php
@@ -47,7 +47,7 @@ include("../functions.php");
             </li>
           </ul>
           <span class="navbar-text">
-            <a class="nav-link" href="../Reg_Login/login.php">Logout</a>
+            <a class="nav-link" href="../Reg_Login/logout.php">Logout</a>
           </span>
         </div>
       </div>
